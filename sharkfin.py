36b3e10a5f1340c04def870a512c21913697070c
SupertallAndy <40362457+SupertallAndy@users.noreply.github.com>
2018-08-10 17:14:44 +0800

    Update sharkfin.py

--- sharkfin.py
+++ sharkfin.py
@@ -7,30 +7,135 @@ Created on Thu Jul 19 19:30:20 2018
 import numpy as np
 from math import exp,sqrt
 import matplotlib.pyplot as plt
+
 r = 0.03 #risk-free rate
 v = 0.2 #volitility
 q = 0 #dividend rate
-t = 0.5 #unit:year
-k = 100
-knock_out = 110
+t = 0.5
+k1 = 100
+k2 = 110
 s = 100
-M = 100 #step
+percentage = 10
+M = 120 #assume that stock price changes 120 times a day
 times = 1000 #run how many times
-dt = t/M
 
-result = []
-for i in range(times):
-    path = []
-    path.append(s)
-    for i in range(M-1):
-        path.append(0)
-    for j in range(1,M):        
-        random_seed = np.random.lognormal((r-q-v*v*0.5)*dt,v*sqrt(dt))
-        path[j] = path[j-1] * random_seed
-        if path[j] > knock_out:
-            result.append(path)
-            break            
-    result.append(path)
+def range_accrue(r,v,q,t,k1,k2,s,percentage,M,times):
+    trading_days = int(252*t)
+    dt = t/(trading_days*M)
+    result = []
+    payoff_list = []
+    for i in range(times):
+        count = 0
+        path = []    
+        for j in range(trading_days+1):
+            if j == 0:
+                path.append(s)
+            else:
+                k = path[j-1]
+                for i in range(M):
+                    random_seed = np.random.lognormal((r-q-v*v*0.5)*dt,v*sqrt(dt))
+                    k *= random_seed
+                #print(k)
+                if k >= k1 and k <= k2:
+                    count += 1
+                path.append(k)
+        payoff_list.append(count/trading_days*percentage)
+        result.append(path)
 
-sharkfin = exp(-r*t)*np.sum([max(path[-1]-k,0) for path in result])/times
-print('the price of the sharkfin is:', sharkfin)
+    price = exp(-r*t) * np.average(payoff_list)
+    #print('the price of the range_accrue is:', price)
+    return price
+
+#range_accrue(0.03,0.3,0,0.5,100,110,100,10,120,1000)
+
+def theta(r,v,q,k1,k2,s,percentage,M,times,lower_bound,upper_bound,step):
+    x_axis = []
+    price = []
+    t = lower_bound
+    while t <= upper_bound:
+        x_axis.append(t)        
+        price.append(range_accrue(r,v,q,t,k1,k2,s,percentage,M,times))
+        t += step  
+    plt.subplot(211)
+    plt.plot(x_axis,price)
+    plt.title('Greeks of range accrue')
+    plt.grid(True)
+    plt.xlabel('maturity')
+    plt.ylabel('price')
+    plt.tight_layout()
+
+def vega(r,q,t,k1,k2,s,percentage,M,times,lower_bound,upper_bound,step):
+    x_axis = []
+    price = []
+    v = lower_bound
+    while v <= upper_bound:
+        x_axis.append(v)        
+        price.append(range_accrue(r,v,q,t,k1,k2,s,percentage,M,times))
+        v += step  
+    plt.subplot(212)
+    plt.plot(x_axis,price)
+    plt.grid(True)
+    plt.xlabel('volatility')
+    plt.ylabel('price')
+
+def upper_limit(r,v,q,t,k1,s,percentage,M,times,lower_bound,upper_bound,step):
+    x_axis = []
+    price = []
+    k2 = lower_bound
+    while k2 <= upper_bound:
+        x_axis.append(k2)        
+        price.append(range_accrue(r,v,q,t,k1,k2,s,percentage,M,times))
+        k2 += step  
+    plt.subplot(211)
+    plt.title('sensitivity of range')
+    plt.plot(x_axis,price)
+    plt.grid(True)
+    plt.xlabel('upper_limit')
+    plt.ylabel('price')
+    plt.tight_layout()
+
+def lower_limit(r,v,q,t,k2,s,percentage,M,times,lower_bound,upper_bound,step):
+    x_axis = []
+    price = []
+    k1 = lower_bound
+    while k1 <= upper_bound:
+        x_axis.append(k1)        
+        price.append(range_accrue(r,v,q,t,k1,k2,s,percentage,M,times))
+        k1 += step  
+    plt.subplot(212)
+    plt.plot(x_axis,price)
+    plt.grid(True)
+    plt.xlabel('lower_limit')
+    plt.ylabel('price')
+
+def adjusted_underlying_asset(S,Cash_percent,F,D,I,r,T):
+    return S * Cash_percent + S * (1-Cash_percent) *(F+D)/I *exp(-r*T)
+
+def adjusted_rangeaccrue(S,Cash_percent,F,D,I,r,T,K1,K2,sigma,M,times):
+    S_0 = adjusted_underlying_asset(S,Cash_percent,F,D,I,r,T)
+    return range_accrue(r,sigma,D,T,K1,K2,S_0, 10, M,times)
+
+def basis_to_price(S,Cash_percent,D,I,r,T,K1,K2,sigma,M,times,lower,upper,step):
+    x_axis = []
+    price = []
+    v = lower
+    while v >= upper:
+        x_axis.append(v)
+        F = I + v        
+        price.append(adjusted_rangeaccrue(S,Cash_percent,F,D,I,r,T,K1,K2,sigma,M,times))
+        v += step  
+    #plt.subplot(212)
+    plt.plot(x_axis,price)
+    plt.grid(True)
+    plt.xlabel('basis')
+    plt.ylabel('price')
+
+basis_to_price(95,0.0121,0,3500,0.02,0.25,100,110,0.2,120,10000,-5,-50,-5)
+basis_to_price(100,0.0121,0,3500,0.02,0.25,100,110,0.2,120,10000,-5,-50,-5)
+basis_to_price(105,0.0121,0,3500,0.02,0.25,100,110,0.2,120,10000,-5,-50,-5)
+basis_to_price(110,0.0121,0,3500,0.02,0.25,100,110,0.2,120,10000,-5,-50,-5)
+basis_to_price(115,0.0121,0,3500,0.02,0.25,100,110,0.2,120,10000,-5,-50,-5)
+#upper_limit(0.03,0.3,0,0.5,100,100,10,120,1000,110,130,1)  
+#lower_limit(0.03,0.3,0,0.5,110,100,10,120,1000,80,100,1)  
+#theta(0.03,0.3,0,100,110,95,10,120,1000,1/12,1,1/12) 
+#vega(0.03,0,0.5,100,110,95,10,120,1000,0.05,1,0.05)   
